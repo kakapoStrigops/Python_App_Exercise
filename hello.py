521b18c92044f0d5cc2c99723ee770062089506d
kakapoStrigops <emi.yamada1109@gmail.com>
2022-11-11 22:34:57 +0900

    [update]いまにゅ ~30:00

--- hello.py
+++ hello.py
@@ -2,10 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+html = '''
+<h1>Hello, World!</h1>
+<ul>
+    <li>箇条書き1</li>
+    <li>箇条書き2</li>
+    <li>箇条書き3</li>
+</ul>
+'''
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return html
 
-@app.route("/japan/<city>")
-def hello_japan(city):
-    return f"<p>Hello, { city } in Japan!</p>"
