0f11d27109fbd233a89c6e68b3db9331df0955eb
kakapoStrigops <emi.yamada1109@gmail.com>
2022-11-11 15:38:43 +0900

    [update]いまにゅ~20:00

--- hello.py
+++ hello.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/japan")
+def hello_japan():
+    return "<p>Hello, Japan!</p>"
+
+@app.route("/america")
+def hello_america():
+    return "<p>Hello, America!</p>"
