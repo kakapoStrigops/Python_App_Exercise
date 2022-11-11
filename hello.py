2735fc84f25f25b997f24c1a455e053ef1e606eb
kakapoStrigops <emi.yamada1109@gmail.com>
2022-11-11 22:48:00 +0900

    [update]いまにゅ ~34:00

--- hello.py
+++ hello.py
@@ -1,17 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-html = '''
-<h1>Hello, World!</h1>
-<ul>
-    <li>箇条書き1</li>
-    <li>箇条書き2</li>
-    <li>箇条書き3</li>
-</ul>
-'''
-
 @app.route("/")
 def hello_world():
-    return html
+    return render_template('hello.html')
 
