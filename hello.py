926178c8aa89418b880d41120976c32eb7eefb4e
kakapoStrigops <emi.yamada1109@gmail.com>
2022-11-11 22:19:42 +0900

    [update]いまにゅ ~27:00

--- hello.py
+++ hello.py
@@ -6,15 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/japan/tokyo")
-def hello_tokyo():
-    return "<p>Hello, tokyo in Japan!</p>"
-
-@app.route("/japan/nara")
-def hello_nara():
-    return "<p>Hello, nara in Japan!</p>"
-
-@app.route("/japan/saitama")
-def hello_saitama():
-    return "<p>Hello, saitama in Japan!</p>"
-
+@app.route("/japan/<city>")
+def hello_japan(city):
+    return f"<p>Hello, { city } in Japan!</p>"
